ff2f839eeb6c31443791c38f94005dab34a5b1b9
Fyodor Sizov <fyodorsizov@Fyodors-MacBook-Air.local>
2022-03-13 01:00:22 +0300

    upd

--- dist/game.js
+++ dist/game.js
@@ -76,8 +76,9 @@ class Game {
 
         this.pressed[key] = true;
 
-        for (var ky in Object.keys(this.pressed)) {
-            console.log(ky)
+        pressedKeys = Object.keys(this.pressed);
+        for (var i in pressedKeys) {
+            ky = pressedKeys[i];
             if (ky == 'ArrowUp') {
                 if (this.sprites.npc1.y > 0) this.sprites.npc1.y -= this.me.physical.speed;
             }
